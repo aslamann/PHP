62141cf03e25929a01da2a0d9e263be90994aee9
aslamann <aslaman@roc-dev.com>
2019-01-15 11:17:40 +0100

    php1

---
+++ h01/page.php
@@ -0,0 +1,5 @@
+<?php
+echo "Hello world";
+?>
+
+<a href="../index.php">Terug naar de inhoudsopgave</a>
--- index.php
+++ index.php
@@ -1,2 +1,13 @@
-<?php
-echo "Hello world";
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, usser-scalable-no, in">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>inhoudsopgave cursus PHP</title>
+</head>
+<body>
+<a href="h01/page.php">Opgave hoofdstuk 1</a>
+</body>
+</html>
