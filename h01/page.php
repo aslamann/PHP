0138abb74fe33241fe094d3c4f6f0da8129df6e6
aslamann <aslaman@roc-dev.com>
2019-01-29 11:42:42 +0100

    php2

--- h01/page.php
+++ h01/page.php
@@ -1,5 +1,46 @@
 <?php
-echo "Hello world";
-?>
+$a = "Ik heb niet gefaald.";
+$b = " die niet werken.";
+$c = 10000;
 
+echo $a." Ik heb alleen ".$c." manieren gevonden".$b;
+?>
+</br>
+<i>-Thomas Edison</i>
+</br>
+</br>
+<?php
+$a = " die geen fout";
+$b = "en";
+$c = "niet.";
+
+echo "Mens".$b.$a.$b." mak".$b.", werken ".$c;
+?>
+</br>
+<i>- Albert Piesman(KLM)</i>
+</br>
+</br>
+<?php
+$a = "ry";
+$b = ", but";
+$c = "fail";
+
+echo "T".$a." and ".$c.$b."never ".$c."to t".$a;
+?>
+</br>
+<i>-Tyra Banks</i>
+</br>
+</br>
+<?php
+$a = "\"omgaan";
+$c = " met ";
+$b = "teleu";
+
+echo "De cursus".$a.$c.$b."rstellingen\""." kan vanavond helaas niet doorgaan.";
+?>
+</br>
+<i>- Herman Finkers</i>
+
+</br>
+</br>
 <a href="../index.php">Terug naar de inhoudsopgave</a>
