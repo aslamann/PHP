514a9422dac419a4dc2b03dfaceecbe251269b72
aslamann <aslaman@roc-dev.com>
2019-02-14 11:21:06 +0100

    php2

---
+++ h02/page2.1.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Anouk
+ * Date: 14-2-2019
+ * Time: 10:48
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Opdracht 2.1</title>
+    <style>
+        body {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+<?php
+for($i = 0; $i <=20; $i++) {
+    for($j = 0; $j<$i; $j++) {
+        echo"*";
+    }
+    echo "*<br>";
+}
+?>
+</body>
+</html>
+
+<br>
+<br>
+<br>
+---------------------------------------------------
+<br>
+<br>
+<a href="../h02/page2.php">Terug naar pagina 1</a>
+<br>
+<a href="../index.php">Terug naar de inhoudsopgave</a>
+
--- h02/page2.php
+++ h02/page2.php
@@ -9,13 +9,13 @@
 ?>
 <html>
 <head>
-    <title>PHP Opdrcht 2</title>
+    <title>PHP Opdrcht 2.1</title>
 </head>
 <body>
 <?php
-//for($i = 0; $i<9 ;$i++){
-//    echo "<img src='apen/aap".$i.".jpg'>";
-//}
+for($i = 0; $i<9 ;$i++){
+    echo "<img src='apen/aap".$i.".jpg'>";
+}
 
 //$i = 1;
 //while($i<=9) {
@@ -31,6 +31,8 @@
 ?>
 
 <br>
+<br>
+<a href="../h02/page2.1.php">naar opdracht 2.1</a>
 <br>
 <a href="../index.php">Terug naar de inhoudsopgave</a>
 </body>
