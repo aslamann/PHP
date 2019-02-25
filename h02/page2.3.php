477d4cdfe719cb7e3d3b452d9e2845807bb7f86f
aslamann <aslaman@roc-dev.com>
2019-02-25 12:44:14 +0100

    php2

--- h02/page2.3.php
+++ h02/page2.3.php
@@ -2,26 +2,18 @@
 /**
  * Created by PhpStorm.
  * User: Anouk
- * Date: 14-2-2019
- * Time: 11:24
+ * Date: 25-2-2019
+ * Time: 10:44
  */
 ?>
 
 
-<?php
-for ($x = 35; $x >= 25; $x--) {
-    echo "hoppelepee: $x <br>";
-}
-
-?>
 
 
-<br>
+
 ---------------------------------------------------
 <br>
 <br>
 <a href="../index.php">Terug naar de inhoudsopgave</a>
 <br>
 <a href="../h02/page2.php">Terug naar pagina 1</a>
-<br>
-<a href="../h02/page2.3.php">Pagina 3</a>
