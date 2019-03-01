0252127fbdbae45e7656121b2eb7762cff37fb3d
aslamann <aslaman@roc-dev.com>
2019-03-01 13:40:56 +0100

    php2

--- h02/page2.4.php
+++ h02/page2.4.php
@@ -17,14 +17,20 @@
 </head>
 <body>
 <?php
-$leeftijd = 66;
+$leeftijd = 64;
 $bedrag = 10;
 if ($leeftijd > 65) {
     $bedrag = $bedrag * 0.5;
 }
+
+if ($leeftijd <= 12) {
+    $bedrag = $bedrag * 0.5;
+
+}
+if ($leeftijd < 3) {
+    $bedrag = 0;
+}
 echo $bedrag;
-
-
 ?>
 </body>
 </html>
