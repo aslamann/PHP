e2486fdc1fa784d58de436d4cd70af63d377eab7
aslamann <aslaman@roc-dev.com>
2019-03-01 13:52:42 +0100

    php2

--- index.php
+++ index.php
@@ -10,7 +10,21 @@
 <body>
 <a href="h01/page.php">Opgave hoofdstuk 1</a>
 <br>
+-----------------------------------------------------
+<br>
 <a href="h02/page2.php">Opgave hoofdstuk 2</a>
-
+<br>
+<a href="h02/page2.1.php">Opgave 2.1</a>
+<br>
+<a href="h02/page2.2.php">Opgave 2.2</a>
+<br>
+<a href="h02/page2.3.php">Opgave 2.3</a>
+<br>
+<a href="h02/page2.4.php">Opgave 2.4</a>
+<br>
+<a href="h02/page2.5.php">Opgave 2.5</a>
+<br>
+-----------------------------------------------------
+<br>
 </body>
 </html>
