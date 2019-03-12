4b6026596e56491574fcd16a719500752e48c3e3
aslamann <aslaman@roc-dev.com>
2019-03-12 07:40:15 +0100

    php2

---
+++ h03/page 3.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Anouk
+ * Date: 4-3-2019
+ * Time: 12:27
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP opdracht 3</title>
+    <style>
+
+    </style>
+</head>
+<body>
+<ul>
+    <?php
+    $C = 30;
+    $F=($C*1.8)+32;
+    echo $F;
+    ?>
+</ul>
+</body>
+</html>
---
+++ h03/page3.1.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Anouk
+ * Date: 12-3-2019
+ * Time: 07:36
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP opdracht 3.1</title>
+    <style>
+
+    </style>
+</head>
+<body>
+<ul>
+
+</ul>
+</body>
+</html>
+
+<br>
+<br>
+---------------------------------------------------
+<br>
+<br>
+<a href="../index.php">Terug naar de inhoudsopgave</a>
+<br>
+<a href="../h03/page%203.php">Terug naar pagina 1</a>
+<br>
+
